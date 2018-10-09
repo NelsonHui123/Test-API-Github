502917b555b469cef2620bc3fcd48f6079ea1637
Nelson Hui <nelson.hui123@gmail.com>
2018-10-09 22:33:02 +0200

    Double fetch Promise

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     super(props)
     this.state = {
       repos: [],
-      languages: []
     }
   }
   componentWillMount = () => {
@@ -15,23 +14,38 @@ class App extends Component {
   }
 
   getRepos = () => {
+    
+    //Premier fetch pour recupérer tout les repos sous forme d'un tableau d'objets
     fetch ('https://api.github.com/users/NelsonHui123/repos')
-      .then(result => result.json())
-      .then(data => {
-        this.setState({
-          repos: data
-        })
-        return data;
-      })
-      .then(data => data.map(x=> {
-        fetch(x.languages_url)
-        .then(result => result.json())
-        .then(data => {
-          this.setState({
-            languages: this.state.languages.concat(data)
-          })
-        })
-      }))
+    
+    .then(result => result.json())
+    
+    //--------------------------------RAJOUT D'UNE CLEE 'language_stat' DANS CHAQUE OBJET DU TABLEAU-------------------------------------------------
+    
+      //On vient stocker dans une constante l'ensemble des nouveaux fetch (pour recevoir l'ojbet contenant les proportions des languages) sous forme d'un tableau,
+      .then(repoArr => {
+        const promises = repoArr.map(
+          repoSingle => fetch(repoSingle.languages_url)
+
+            .then(result => result.json())
+        )
+
+        //Ici on utilise Promise.all pour pouvoir recevoir le resultat de toutes les requètes des API dans le bon ordre (on est obligé de faire ça pour pouvoir lier un repo avec sa proportion des languages correspondante)
+        return Promise.all(promises)
+
+
+        //Ensuite on vient mapper le tableau contenant les proportions de languages (avec le paramètre index dans le map), et on va rajouter la
+        // clee 'language_stat' dans chaque repo en utilisant l'idx du tableau de proportion car on sait qu'ils sont dans le même ordre
+          .then(languages => languages.map(
+            (language, idx) => Object.assign(repoArr[idx], {language_stat: language})
+          ))
+
+          //Enfin on balance tout ça dans le state
+          .then(repos => this.setState({repos:repos}))
+
+
+      //on peut aussi utiliser async et await pour écrire du code assynchrone avec la syntaxe du code synchrone
+    })
   }
 
   render() {
